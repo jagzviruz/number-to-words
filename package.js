4f62b3ba37e3bd68bdef8da2a18c6aef9a63f9e8
Jagadish, K <k.jagdish@gmail.com>
2016-03-01 11:57:16 +0530

    Reduced summary for submitting to atmosphere

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'jagzviruz:number-to-words',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: 'It contains some util methods for converting numbers into words, ordinal words and ordinal numbers. It wraps the npm mapckage available at https://github.com/marlun78/number-to-words',
+  summary: 'It contains some util methods for converting numbers into words, ordinal words and ordinal numbers.',
   // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/jagzviruz/number-to-words',
   // To avoid submitting documentation, set this field to null.
